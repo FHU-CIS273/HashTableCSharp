64c6081ab56129aaf0f9f7437a3f41422cc930c6
Kenan Casey <kenancasey@gmail.com>
2019-10-21 10:24:13 -0500

    Add HashTableChaining updates

--- HashTableCSharp/HashTableChaining.cs
+++ HashTableCSharp/HashTableChaining.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace HashTable
 {
-    public class HashTableChaining
+    public class HashTableChaining<K, V> : IHashTable<K,V>
     {
+        private LinkedList< Bucket<K,V> >[] listsOfBuckets;
+
+        private const double MaxLoadFactor = 0.75;
+
+        private int capacity = 10;
+
+        public double LoadFactor
+        {
+            get
+            {
+                int fullBuckets = buckets.Count(b => b.State == BucketState.Full);
+
+                return (double)fullBuckets / buckets.Length;
+            }
+        }
+
         public HashTableChaining()
         {
         }
